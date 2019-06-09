a938b35ebf829bf2bef29d8f62cc8370a7877a58
sven-hoek <herrlauch@gmail.com>
2019-06-09 23:00:03 +0300

    Add documentation, start preparing to compare full embodiments.

--- scripts/gazebo_env.py
+++ scripts/gazebo_env.py
@@ -24,19 +24,39 @@ class GazeboEnvFullPanda(gym.Env):
     """
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, step_size, bagfile):
+    def __init__(self, step_size, bagfile, duration):
         super(GazeboEnvFullPanda, self).__init__()
         self.current_step = 0
+        self.done = False
         self.step_size = step_size
-
+        self.duration = duration
+        self._received_first_ldata = False
+        self.l_position = [0.0] * 7
+        self.l_velocity = [0.0] * 7
         self.e_position = [0.0] * 7
         self.e_velocity = [0.0] * 7
-        self.e_effort = [0.0] * 7
 
         self.bagfile = rosbag.Bag(bagfile)
         self.bagfile_start_time = rospy.Time(self.bagfile.get_start_time())
         if DEBUG: print("Bagfile opened.")
 
+        rospy.init_node('gym_environment_wrapper')
+        if DEBUG: print("ROS node initialized.")
+
+        self._joint_states_subscriber = rospy.Subscriber('panda1/joint_states', JointState, self._joint_state_callback, queue_size=1)
+        if DEBUG: print("Joint State Subscriber registered.")
+
+        self._command_publisher = rospy.Publisher('panda1/effort_jointgroup_controller/command', Float64MultiArray, queue_size=1)
+        if DEBUG: print("Command Publisher registered.")
+        self._command = Float64MultiArray()
+        self._command_zero = Float64MultiArray()
+        self._command_zero.data = [0.0] * 7
+
+        self._pause_gazebo_service = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self._unpause_gazebo_service = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self._reset_gazebo_service = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self._switch_controller_service = rospy.ServiceProxy('panda1/controller_manager/switch_controller', SwitchController)
+        if DEBUG: print("Services registered.")
 
         # TODO: Find good value for max reward
         self.reward_range = (-(4*174 + 72), 0)
@@ -47,12 +67,16 @@ class GazeboEnvFullPanda(gym.Env):
             high=np.array([87, 87, 87, 87, 12, 12, 12]),
             dtype='float32')
 
-        # Respectively: t_joint_angles
+        # Respectively: l_joint_angles, l_joint_vels, t_joint_angles, t_joint_vels
         self.observation_space = gym.spaces.Box(
             low=np.array([[-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973],
-                         [-87, -87, -87, -87, -12, -12, -12]]),
+                          [-2.1750, -2.1750, -2.1750, -2.1750, -2.6100, -2.6100, -2.6100],
+                          [-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973],
+                          [-2.1750, -2.1750, -2.1750, -2.1750, -2.6100, -2.6100, -2.6100]]),
             high=np.array([[2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973],
-                          [87, 87, 87, 87, 12, 12, 12]]),
+                           [2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100],
+                           [2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973],
+                           [2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100]]),
             dtype='float32')
 
     def __del__(self):
@@ -60,24 +84,63 @@ class GazeboEnvFullPanda(gym.Env):
         self.bagfile.close()
 
     def reset(self):
-        if DEBUG: print("Reset method start")
+        """
+        Resets the environment. In order for the ROS controller to function properly after the reset, the desired effort
+        will to be set to zero for all joints beforehand. After resetting it waits to receive the first joint_state
+        message to reset the state of the learner.
+        :return: Current observation.
+        """
+        if DEBUG: print("Reset method start.")
+        self._command_publisher.publish(self._command_zero)
+        if DEBUG: print("Zero command published.")
+        self._reset_gazebo_service()
+        if DEBUG: print("Gazebo reset.")
+        self._unpause_gazebo_service()
+        if DEBUG: print("Gazebo unpaused.")
+        self._restart_joint_state_controller()
+        if DEBUG: print("Joint State Controller resetted.")
+        self._received_first_ldata = False
+        while self._received_first_ldata is False:
+            # TODO: better a sleep function? Resolve issues with ROS clock?
+            pass
+        self._pause_gazebo_service()
+        if DEBUG: print("Gazebo paused.")
         self.current_step = 0
-        self.e_position, self.e_velocity, self.e_effort = self._get_expert_state_from_bagfile(self.current_step)
+        if DEBUG: print("Getting expert state from bagfile.")
+        try:
+            self.e_position, self.e_velocity = self._get_expert_state_from_bagfile(self.current_step)
+            self.done = False
+        except StopIteration:
+            if DEBUG: print("StopIteration Exception! Setting done->False")
+            self.done = True
         if DEBUG: print("Reset method end.")
-        return [self.e_position, self.e_effort]
+        return [self.l_position, self.l_velocity, self.e_position, self.e_velocity]
 
     def step(self, action):
+        """
+        Executes the given action by publishing the joint effort command to the ROS controller. In order to receive the
+        joint_states messages properly, the joint_state_controller needs to be restarted.
+        :param action: A list of joint efforts to send to the joints of the learner.
+        :return: The current state/observation, the immediate reward and the 'done' flag.
+        """
+        self._unpause_gazebo_service()
+        self._restart_joint_state_controller()
+        self._command.data = action
+        self._command_publisher.publish(self._command)
+        rospy.sleep(self.step_size)
+        self._pause_gazebo_service()
         self.current_step += 1
         done = False
 
-        # For testing: Just compare taken action with action from teacher ~behavioral cloning
         try:
-            self.e_position, self.e_velocity, self.e_effort = self._get_expert_state_from_bagfile(self.current_step-1)
+            self.e_position, self.e_velocity = self._get_expert_state_from_bagfile(self.current_step)
         except StopIteration:
+            if DEBUG: print("StopIteration Exception! Setting done->False")
             done = True
 
-        observation = [self.e_position, self.e_effort]
+        # TODO Calculate reward
         reward = -np.linalg.norm(np.array(action) - np.array(self.e_effort))
+        observation = [self.l_position, self.l_velocity, self.e_position, self.e_velocity]
 
         if DEBUG2: print("[{}] Comparing (Reward: {})".format(self.current_step, reward))
         if DEBUG2: print(action)
@@ -87,34 +150,62 @@ class GazeboEnvFullPanda(gym.Env):
         return observation, reward, done, {}
 
     def render(self, mode='human', close='False'):
+        # TODO: Plot embodiments
         pass
 
+    def _joint_state_callback(self, joint_state):
+        """
+        Callback function for the joint_state_subscriber. Saves the received position and velocity.
+        :param joint_state: Received data from joint_states topic.
+        """
+        # if DEBUG: print("Callback start")
+        self.l_position = joint_state.position
+        self.l_velocity = joint_state.velocity
+        # if DEBUG: print("Callback end")
+        if not self._received_first_ldata:
+            self._received_first_ldata = True
+
+    def _restart_joint_state_controller(self):
+        """
+        It is necessary to restart the joint state controller each time after unpausing gazebo in order to publish/
+        receive joint_state messages.
+        :return: None
+        """
+        self._switch_controller_service(stop_controllers=['franka_sim_state_controller'],
+                                        strictness=SwitchControllerRequest.BEST_EFFORT)
+        self._switch_controller_service(start_controllers=['franka_sim_state_controller'],
+                                        strictness=SwitchControllerRequest.BEST_EFFORT)
+
     def _get_expert_state_from_bagfile(self, step):
+        """
+        Gets a joint_message to the corresponding time of the given step from the bagfile and extracts the joint
+        position and velocity (representing the state of the expert).
+        :param step: Current step number.
+        :return: Lists containing the joint positions and velocities
+        """
         if DEBUG: print("Getting expert state from bagfile.")
         # Go one step ahead, otherwise data from bag will have a little delay
         step += 1
         expert_joint_state = next(self.bagfile.read_messages(topics=['/panda1/joint_states'],
                                                              start_time=self.bagfile_start_time + rospy.Duration(self.step_size * step),
-                                                             end_time=self.bagfile_start_time + rospy.Duration(10.0)))[1]
-        return expert_joint_state.position, expert_joint_state.velocity, expert_joint_state.effort
+                                                             end_time=self.bagfile_start_time + rospy.Duration(self.duration)))[1]
+        return expert_joint_state.position, expert_joint_state.velocity
 
 
 if __name__ == '__main__':
-    env = GazeboEnvFullPanda(1.0, '../resources/torque_trajectory_002.bag')
+    env = GazeboEnvFullPanda(0.1, '../resources/torque_trajectory_002.bag')
     env.reset()
     if DEBUG: print("We end here.")
     # quit()
-    cumreward = 0
+
     with open('../resources/torque_trajectory_002_commands.csv', newline='\n') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         for line in csv_reader:
             obs, reward, done, _ = env.step(line)
-            cumreward += reward
             if done: break
             #print(line)
             #print(env._get_expert_state_from_bagfile(env.current_step)[2])
             print("")
-    print("\nCumulative Reward: {}".format(cumreward))
 
     # with rosbag.Bag('../resources/torque_trajectory_002.bag') as bagfile:
     #     start_time = rospy.Time(bagfile.get_start_time())
